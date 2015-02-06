5d95190d2ad8fa6446d75753692be82adc63a260
pqwy <david@numm.org>
2015-02-06 21:15:02 +0000

    add public addr to config.ml

--- config.ml
+++ config.ml
@@ -1,9 +1,16 @@
 open Mirage
 
+let address addr nm gw =
+  let f = Ipaddr.V4.of_string_exn in
+  { address = f addr ; netmask = f nm ; gateways = [f gw] }
+
+let server = address "46.246.46.123" "255.255.255.0" "46.246.46.1"
+
 let net =
   match get_mode () with
-  | `Xen  -> direct_stackv4_with_dhcp default_console tap0
   | `Unix -> direct_stackv4_with_dhcp default_console (netif "tap9")
+  | `Xen  -> direct_stackv4_with_dhcp default_console tap0
+(*   | `Xen  -> direct_stackv4_with_static_ipv4 default_console tap0 server *)
 
 
 let kv = crunch "disk"
