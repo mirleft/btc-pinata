08ae4cc08f05d740860f1ba17c0515f538750716
pqwy <david@numm.org>
2015-02-06 21:25:39 +0000

    fix bounded logger

--- logger.ml
+++ logger.ml
@@ -2,11 +2,13 @@ open Lwt
 open V1
 open V1_LWT
 
+let (&.) f g x = f (g x)
+
 module Make (C : CONSOLE) (S : STACKV4) (Clock : CLOCK) : sig
   val init : C.t ->  S.t -> unit
   val log : string -> unit
   val log_ext : string -> (Ipaddr.V4.t * int) -> string -> unit
-  val trace : (Ipaddr.V4.t * int) -> Sexplib.Sexp.t -> unit
+  val tracing : (Ipaddr.V4.t * int) -> ((Sexplib.Sexp.t -> unit) -> 'a Lwt.t) -> 'a Lwt.t
 end =
 struct
 
@@ -16,17 +18,32 @@ struct
   and log_port    = 12345
   and source_port = 12345
 
-
   type message =
-      Log   of string * float * string
-    | Trace of string * (Ipaddr.V4.t * int) * int * float * Sexplib.Sexp.t
+    | Log   of string * float * string
+    | Trace of (Ipaddr.V4.t * int) * float * Sexplib.Sexp.t
+
+  let semaphore ~n =
+    let pending = ref 0
+    and cond    = Lwt_condition.create () in
+    let rec v () =
+      if !pending < n then (incr pending ; return_unit)
+      else Lwt_condition.wait cond >> v ()
+    and p () = decr pending ; Lwt_condition.broadcast cond ()
+    and steal () = incr pending
+    in
+    (v, p, steal)
+
+  let (sem_v, sem_p, sem_steal) = semaphore ~n:50
 
   let render = function
-    | Log (tag, time, msg) -> Printf.sprintf "%s%.04f %s\n" tag time msg
-    | Trace (id, (host, port), seq, time, sexp) ->
-        Printf.sprintf "[trace] %s %s:%d %d %.04f %s\n"
-          id (Ipaddr.V4.to_string host) port seq time
-          (Sexplib.Sexp.to_string sexp)
+    | Log (tag, time, msg) ->
+        Cstruct.of_string @@
+          Printf.sprintf "%s%.04f %s\n" tag time msg
+    | Trace ((host, port), time, sexp) ->
+        Cstruct.of_string @@
+          Printf.sprintf "[trace] %s:%d %.04f %s\n"
+            (Ipaddr.V4.to_string host) port time
+              (Sexplib.Sexp.to_string_mach sexp)
 
   let (chan, push) = Lwt_stream.create ()
 
@@ -39,43 +56,37 @@ struct
 
   let init c s =
     let t = S.(tcpv4 s) in
-    let rec send msg = function
+    let rec send cs = function
       | `Disconnected n ->
           OS.Time.sleep n >>
           TCP.create_connection t (log_ip, log_port) >>= (function
-            | `Ok tcp  -> send msg (`Connected tcp)
-            | `Error _ -> send msg (`Disconnected (n *. 2.)))
+            | `Ok tcp  -> send cs (`Connected tcp)
+            | `Error _ -> send cs (`Disconnected (n *. 2.)))
       | `Connected tcp ->
-          let cs = Cstruct.of_string (render msg) in
           TCP.write_to ~timeout:5. tcp cs >>= (function
-            | `Ok () -> return (`Connected tcp)
-            | _      -> async (fun _ -> TCP.close tcp); send msg (`Disconnected 1.))
+            | `Ok () -> sem_p () ; return (`Connected tcp)
+            | _      -> async (fun _ -> TCP.close tcp); send cs (`Disconnected 1.))
     in
-    async @@ fun () -> Lwt_stream.fold_s send chan (`Disconnected 0.1)
+    async @@ fun () -> Lwt_stream.fold_s (send &. render) chan (`Disconnected 0.1)
 
   let log msg =
+    sem_steal () ;
     push (Some (Log ("", Clock.time(), msg)))
 
   let log_ext tag (ip, port) =
     let tag =
       Printf.sprintf "[%s] %s:%d " tag (Ipaddr.V4.to_string ip) port in
-    fun msg -> push (Some (Log (tag, Clock.time(), msg)))
+    fun msg ->
+      sem_steal () ;
+      push (Some (Log (tag, Clock.time(), msg)))
 
-  let string_of_cs_hex cs =
-    let buf = Buffer.create 32 in
-    let emit x =
-      Buffer.add_char buf (Char.chr (x + if x < 10 then 48 else 87)) in
-    for i = 0 to Cstruct.len cs - 1 do
-      let n = Cstruct.get_uint8 cs i in
-      emit (n land 0x0f) ; emit ((n land 0xf0) lsr 4)
-    done ;
-    Buffer.contents buf
-
-  let trace peer =
-    let id = string_of_cs_hex (Nocrypto.Rng.generate 8)
-    and n   = ref 0 in
-    fun sexp ->
-      incr n ;
-      push (Some (Trace (id, peer, !n, Clock.time(), sexp)))
+  let tracing peer f =
+    let traces = ref [] in
+    sem_v () >>
+    try_lwt f (fun x -> traces := x :: !traces)
+    finally
+      let sexp = Sexplib.Sexp.List (List.rev !traces) in
+      push (Some (Trace (peer, Clock.time (), sexp))) ;
+      return_unit
 
 end
