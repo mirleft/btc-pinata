48ede0590af1240c0d43a20532ae8b8448b591bb
pqwy <david@numm.org>
2015-02-07 18:10:17 +0000

    some more wording

--- page.ml
+++ page.ml
@@ -79,9 +79,8 @@ let content ca_root =
 
       <h3>You have reached the BTC Piñata</h3>
 
-      <p>BTC Piñata knows the private key to the 10 bitcoins of address $a_chain$.
-      If you break the piñata, you get to keep what's inside. This challenge runs
-      until mid March 2015.</p>
+      <p>BTC Piñata knows the private key to the bitcoin address $a_chain$. If
+      you break the piñata, you get to keep what's inside.</p>
 
       <p>Here are the rules of the game:</p>
 
@@ -104,7 +103,7 @@ let content ca_root =
 
       <p>And here's the kicker: in both the client and server roles, piñata
       requires the other end to present a certificate. Authentication is performed
-      using standard $a_path_val$ with a single certificate as trust
+      using standard $a_path_val$ with a single certificate as the trust
       anchor. And no, you can't have its key.</p>
 
       <p>It follows that it should be impossible to successfully establish a TLS
@@ -115,9 +114,10 @@ let content ca_root =
       directly on Xen, and is using native OCaml $a_tls$ and $a_x509$
       implementations.</p>
 
-      <p>The $a_full_list$ of installed software and a toy $a_unikernel$ (no secrets
-      included) are available, there is no need to use your automated tools on this
-      server - run your own instead.</p>
+      <p>The $a_full_list$ of installed software and a toy $a_unikernel$ (no
+      secrets included) are available. There is no need to use your automated
+      tools on piñata - roll your own instead. This challenge runs until mid
+      March 2015.</p>
 
       <p>Bitcoins and the hosting for this challenge are sponsored by
       $a_ipredator$, a friendly virtual private network provider!</p>
@@ -125,8 +125,9 @@ let content ca_root =
       <p>This is the CA:</p>
       $ca$
 
-      <p>If you have any results or further questions, don't hesitate to contact us
-      (contact eMail address in certificates).</p>
+      <p>If you have any results or further questions, don't hesitate to contact
+      us. Address is in the certificate. </p>
+
     </div>
 
   >>
@@ -134,5 +135,3 @@ let content ca_root =
 let render ca_root =
   Cstruct.of_string @@ Html.to_string @@
     wrap_body ~title:"BTC Piñata" ~body:(content ca_root)
-
-
